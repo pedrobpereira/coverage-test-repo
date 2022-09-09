414930f142a3ea63b444f2a29937bb1c21ac3b36
Pedro Pereira <pedro.pereira@codacy.com>
2022-09-09 15:25:41 +0100

    decrease coverage

--- src/main/java/Whatever.java
+++ src/main/java/Whatever.java
@@ -1,7 +1,13 @@
 public class Whatever {
 
-    public String returnWhat(String what) {
-        return what;
+    public String returnWhat(String what, boolean whatIf) {
+
+        if (whatIf) {
+            return what;
+        }
+        else {
+            return "nothing";
+        }
     }
 
     public void printWhat(String what) {
