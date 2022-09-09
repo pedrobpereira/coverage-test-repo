99bd6dd0eb4d03fb6a9ab9d402c2dcec39565331
Pedro Pereira <pedro.pereira@codacy.com>
2022-09-09 17:37:50 +0100

    more coverage changes

--- src/main/java/Whatever.java
+++ src/main/java/Whatever.java
@@ -11,7 +11,7 @@ public class Whatever {
     }
 
     public void printWhat(String what, boolean whatIf) {
-        if (whatIf) {
+        if (whatIf && what.isEmpty()) {
         System.out.println(what);
         } else {
             System.out.println("nothing");
