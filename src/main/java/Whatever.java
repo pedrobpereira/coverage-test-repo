9b7b34eb9e572d5585b06a63b197e89464dde651
Pedro Pereira <pedro.pereira@codacy.com>
2022-09-09 15:11:42 +0100

    initial commit

---
+++ src/main/java/Whatever.java
@@ -0,0 +1,10 @@
+public class Whatever {
+
+    public String returnWhat(String what) {
+        return what;
+    }
+
+    public void printWhat(String what) {
+        System.out.println(what);
+    }
+}
---
+++ src/test/java/WhateverTest.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(JUnit4.class)
+public class WhateverTest {
+
+    private Whatever whatever = new Whatever();
+
+    @Test
+    public void returnWhatReturnsInput() {
+        // Arrange
+        String what = "what";
+
+        //Act
+        String result = whatever.returnWhat(what);
+
+        //Assert
+        assertEquals(what, result);
+    }
+
+}
