a3839aaf7ae612a9d58e9c5462f6e9afb232c1de
Pedro Pereira <pedro.pereira@codacy.com>
2022-12-07 12:08:21 +0000

    adds issue 1

--- src/main/java/Whatever.java
+++ src/main/java/Whatever.java
@@ -2,13 +2,14 @@ public class Whatever {
 
     public String returnWhat(String what, boolean whatIf) {
 
-        String what = null;
+        String stuff = null;
 
         if (whatIf) {
             System.out.println();
             System.out.println();
             System.out.println();
             System.out.println();
+
 
 
             return what;
