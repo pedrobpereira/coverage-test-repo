f2891d582e5ad316dc967635789691cbdb8bf51d
Pedro Pereira <pedro.pereira@codacy.com>
2023-01-05 17:20:20 +0000

    whatever

---
+++ src/main/java/Whatever2.java
@@ -0,0 +1,20 @@
+public class Whatever2 {
+
+    public String returnWhat(String what, boolean whatIf) {
+
+        if (whatIf) {
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+
+
+
+
+            return what;
+        }
+        else {
+            return "nothing";
+        }
+    }
+}
---
+++ src/test/java/Whatever2Test.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(JUnit4.class)
+public class Whatever2Test {
+
+    private Whatever2 whatever = new Whatever2();
+
+    @Test
+    public void returnWhatReturnsInput() {
+        // Arrange
+        String what = "what";
+
+        //Act
+        String result = whatever.returnWhat(what, true);
+
+        //Assert
+        assertEquals(what, result);
+    }
+}
