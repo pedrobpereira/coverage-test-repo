34f5f9e495f1816363b6f07897e1afb1edb78545
Pedro Pereira <pedro.pereira@codacy.com>
2023-01-12 15:48:00 +0000

    increases coverage

--- src/test/java/WhateverTest.java
+++ src/test/java/WhateverTest.java
@@ -20,4 +20,16 @@ public class WhateverTest {
         //Assert
         assertEquals(what, result);
     }
+
+    @Test
+    public void returnNothingReturnsInput() {
+        // Arrange
+        String what = "what";
+
+        //Act
+        String result = whatever.returnWhat(what, false);
+
+        //Assert
+        assertEquals("nothing", result);
+    }
 }
