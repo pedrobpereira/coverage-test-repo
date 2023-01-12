70825240b54b2fb9f646362f7107884361c166b5
Pedro Pereira <pedro.pereira@codacy.com>
2023-01-12 17:26:00 +0000

    increases coverage

--- src/main/java/Whatever.java
+++ src/main/java/Whatever.java
@@ -17,16 +17,11 @@ public class Whatever {
 
     public String returnWhat2(String what, boolean whatIf) {
 
-        if (whatIf) {
             System.out.println();
             System.out.println();
             System.out.println();
             System.out.println();
 
             return what;
-        }
-        else {
-            return "nothing";
-        }
     }
 }
