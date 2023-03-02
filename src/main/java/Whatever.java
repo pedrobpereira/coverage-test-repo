acfc3303043c260aa346755703e99e9f74886168
Pedro Pereira <pedro.pereira@codacy.com>
2023-03-02 12:45:25 +0000

    whatever 11112222

--- src/main/java/Whatever.java
+++ src/main/java/Whatever.java
@@ -5,7 +5,6 @@ public class Whatever {
         if (whatIf) {
             System.out.println();
             System.out.println();
-            System.out.println();
 
             return what;
         }
