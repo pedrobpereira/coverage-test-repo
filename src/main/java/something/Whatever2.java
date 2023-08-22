d79898b7a18684a35af2562aa4feb07635f49736
Pedro Pereira <pedro.pereira@codacy.com>
2023-08-22 12:52:25 +0100

    something 4

--- src/main/java/something/Whatever2.java
+++ src/main/java/something/Whatever2.java
@@ -15,17 +15,4 @@ public class Whatever2 {
         }
     }
 
-    public String returnWhat2(String what, boolean whatIf) {
-
-        if (whatIf) {
-            System.out.println();
-            System.out.println();
-
-            return what;
-        }
-        else {
-            return "nothing";
-        }
-    }
-
 }
