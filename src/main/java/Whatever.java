6b13d10ceebaab2110a89709097fc0eb625e881a
Pedro Pereira <pedro.pereira@codacy.com>
2023-10-11 14:17:03 +0100

    good diff 16 + 18

--- src/main/java/Whatever.java
+++ src/main/java/Whatever.java
@@ -19,7 +19,7 @@ public class Whatever {
             return what;
         }
         else {
-            return "nottrret1rehefgqrfuhe2ççççç";
+            return "nottrret1rehefgqrfuhe2çççççăѣ\uD835\uDD20ծềſģȟᎥ\uD835\uDC8Bǩľḿꞑȯ\uD835\uDE31\uD835\uDC5E\uD835\uDDCB\uD835\uDE34ȶ\uD835\uDF84\uD835\uDF08ψ\uD835\uDC99\uD835\uDE06\uD835\uDEA31234567890!@#$%^&*()-_=+[{]};:'\",<.>/?";
         }
     }
 
