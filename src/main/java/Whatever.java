3a504da39e07db17ac947ce540dbdceb5ba7e5cd
Pedro Pereira <pedro.pereira@codacy.com>
2023-10-16 15:53:41 +0100

    good diff 16 + 26

--- src/main/java/Whatever.java
+++ src/main/java/Whatever.java
@@ -19,7 +19,7 @@ public class Whatever {
             return what;
         }
         else {
-            return "nottrret1rehefgqrfuhe2çççççăѣ`|`´´€€ļᵲꜱö";
+            return "nottrret1rehefgqrfuhe2çççăѣ`|`´´€€ļᵲꜱö";
         }
     }
 
