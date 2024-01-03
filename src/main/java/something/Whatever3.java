3d1eeb0c4c6eb39ebe2bd73d9448ab71f312d5d1
Pedro Pereira <pedro.pereira@codacy.com>
2024-01-03 11:36:24 +0000

    adds duplication

---
+++ src/main/java/something/Whatever3.java
@@ -0,0 +1,33 @@
+package something;
+
+public class Whatever3 {
+
+    public String returnWhat(String what, boolean whatIf) {
+
+        if (whatIf) {
+            System.out.println();
+            System.out.println();
+
+            return what;
+        }
+        else {
+            return "nothing";
+        }
+    }
+
+    public String returnWhat2(String what, boolean whatIf) {
+
+        if (whatIf) {
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+
+            return what;
+        }
+        else {
+            System.out.println();
+            return "nothing";
+        }
+    }
+}
