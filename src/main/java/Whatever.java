1348c893e5bfd0d4aaa0093d56d2b5bb2e0e05d7
Pedro Pereira <pedro.pereira@codacy.com>
2024-03-07 15:24:03 +0000

    whatevrr 1

--- src/main/java/Whatever.java
+++ src/main/java/Whatever.java
@@ -10,7 +10,6 @@ public class Whatever {
             System.out.println();
             System.out.println();
             System.out.println();
-            System.out.println();
 
             return what;
         }
