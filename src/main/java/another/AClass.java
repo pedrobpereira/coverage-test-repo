2414af2fc5f1113af53daa7598536421a6762601
Pedro Pereira <pedro.pereira@codacy.com>
2024-04-19 15:57:52 +0100

    adds files

---
+++ src/main/java/another/AClass.java
@@ -0,0 +1,17 @@
+package another;
+
+public class AClass {
+    public String returnWhat(String what, boolean whatIf) {
+
+        if (whatIf) {
+            System.out.println();
+            System.out.println();
+            System.out.println();
+
+            return what;
+        }
+        else {
+            return "nothing";
+        }
+    }
+}
