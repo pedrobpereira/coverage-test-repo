661cc54c9228b2c3a64e469a2ed2d79e4666c0f5
Pedro Pereira <pedro.pereira@codacy.com>
2024-05-09 10:36:35 +0100

    adds line

--- src/main/java/Whatever.java
+++ src/main/java/Whatever.java
@@ -6,6 +6,7 @@ public class Whatever {
             System.out.println();
             System.out.println();
             System.out.println();
+            System.out.println();
 
             return what;
         }
