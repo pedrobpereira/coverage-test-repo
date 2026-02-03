2110397a2c677447d05a6844370659af50d2ba76
Pedro Pereira <pedro.pereira@codacy.com>
2026-02-03 15:03:34 +0000

    empty adds coverage

---
+++ src/test/java/something/WhateverTest.java
@@ -0,0 +1,25 @@
+package something;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(JUnit4.class)
+public class WhateverTest {
+
+    private Whatever whatever = new Whatever();
+
+    @Test
+    public void returnWhatReturnsInput() {
+        // Arrange
+        String what = "what";
+
+        //Act
+        String result = whatever.returnWhat(what, true);
+
+        //Assert
+        assertEquals(what, result);
+    }
+}
