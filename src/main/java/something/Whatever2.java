9690a673a04b3361b37e47c9ebc9421c80fdde98
Pedro Pereira <pedro.pereira@codacy.com>
2026-02-20 14:47:36 +0000

    stuff 1

--- src/main/java/something/Whatever2.java
+++ src/main/java/something/Whatever2.java
@@ -8,13 +8,6 @@ public class Whatever2 {
             System.out.println();
             System.out.println();
             System.out.println();
-            System.out.println();
-            System.out.println();
-            System.out.println();
-            System.out.println();
-            System.out.println();
-            System.out.println();
-            System.out.println();
 
             return what;
         }
