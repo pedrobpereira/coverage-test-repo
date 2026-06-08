c4b58f070e44405e299868c2ad22ac35e924cbd4
Pedro Pereira <pedro.pereira@codacy.com>
2026-06-08 15:07:52 +0100

    stuff 1 - adds file with issues

---
+++ openAiConnector2.py
@@ -0,0 +1,7 @@
+from openai import OpenAI
+client = OpenAI()
+resp = client.responses.create(model="gpt-4.1", input="Hello")
+
+from guardrails import GuardrailsOpenAI
+client = GuardrailsOpenAI(config="guardrails_config.json")
+resp = client.responses.create(model="gpt-4.1", input="Hello")
